9a57c82834cd5d9ae0a6b3185b29d21a3835c3b6
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-04-29 22:00:56 +0530

    java

---
+++ equal.java
@@ -0,0 +1,13 @@
+public class equal {
+    public static void main(String[] args) {
+        String a="dharshu";
+        String b="dharshu";
+        if(a==b){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+        //System.out.println(a>b);
+    }
+}
---
+++ greatest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class greatest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        if(a>b){
+            System.out.println("the greatest number is:"+a);
+        }
+        else if(b>c){
+            System.out.println("the greatest number is:"+b);
+        }
+        else{
+            System.out.println("the greatest number is:"+c);
+        }
+    }
+}
